090ca25f9a9a6e95e7cec1a1fb47df4a0bd33dae
Sbtah <shisastore@protonmail.com>
2023-09-15 17:18:50 +0200

    Refactor api.py

--- scout/client/api.py
+++ scout/client/api.py
@@ -1,9 +1,9 @@
 import asyncio
+import json
 import os
-import time
+
 import httpx
 from dotenv import load_dotenv
-import json
 from utilities.logging import logger
 
 
@@ -12,15 +12,22 @@ load_dotenv()
 
 class TorScoutApiClient:
 
-    def __init__(self, key=os.environ.get('API_KEY'), url=os.environ.get('API_URL')):
+    def __init__(
+            self,
+            key=os.environ.get('API_KEY'),
+            url=os.environ.get('API_URL'),
+    ):
         self.key = key
         self.url = url
         self.logger = logger
         self.RESPONSE_ENDPOINT = f'{self.url}/api/process-response/'
         self.HOME_ENDPOINT = f'{self.url}/api/'
-        self.UNCRAWLED_ENDPOINT = f'{self.url}/api/get-not-crawled/'
 
     async def get(self, url: str):
+        """
+        Sends get request to specified URL.
+        - :arg url: Requested URL.
+        """
         try:
             async with httpx.AsyncClient() as client:
                 response = await client.get(url)
@@ -29,7 +36,12 @@ class TorScoutApiClient:
             self.logger.error(f'(get) Some other exception: {e}')
             raise
 
-    async def post(self, url, data):
+    async def post(self, url: str, data: dict):
+        """
+        Sends post request to specified URL.
+        - :arg url: Requested URL.
+        - :arg data: Dictionary with prepared data.
+        """
         try:
             async with httpx.AsyncClient() as client:
                 response = await client.post(url, json=json.dumps(data))
@@ -39,6 +51,9 @@ class TorScoutApiClient:
             raise
 
     async def get_home(self):
+        """
+        Requests endpoint: api/.
+        """
         try:
             tasks = [asyncio.create_task(
                 self.get(self.HOME_ENDPOINT)
@@ -49,7 +64,10 @@ class TorScoutApiClient:
             self.logger.error(f'(get_home) Some other exception: {e}')
             raise
 
-    async def post_response_data(self, data):
+    async def post_response_data(self, data: dict):
+        """
+        Sends response data to: api/process-response/
+        """
         try:
             tasks = [asyncio.create_task(
                 self.post(self.RESPONSE_ENDPOINT, data=data)
@@ -59,16 +77,3 @@ class TorScoutApiClient:
         except Exception as e:
             self.logger.error(f'(post_response_data) Some other exception: {e}')
             raise
-
-    async def get_not_crawled_website(self):
-        try:
-            tasks = [asyncio.create_task(
-                self.get(self.UNCRAWLED_ENDPOINT)
-            )]
-            responses = await asyncio.gather(*tasks)
-            if responses:
-                return responses[0]
-            return None
-        except Exception as e:
-            self.logger.error(f'(get_not_crawled_website) Some other exception: {e}')
-            return None

993661439b37568d39852f8ef6102e4eecc59fd9
Sbtah <shisastore@protonmail.com>
2023-09-22 20:38:40 +0200

    Update crawl management command to use Launcher logic.

--- scout/core/management/commands/crawl.py
+++ scout/core/management/commands/crawl.py
@@ -4,37 +4,12 @@ import time
 from django.core.management.base import BaseCommand
 from logic.adapters.task import TaskAdapter
 from logic.spiders.crawling_spider import Crawler
+from logic.launchers.launcher import CrawlerLauncher
 
-
-class CrawlerLauncher:
-    """
-    Object representing simple Celery task logic.
-    """
-
-    def __init__(self, *args, **kwargs):
-        self.task_adapter = TaskAdapter()
-        self.crawler = Crawler
-
-    def start(self):
-        """
-        Entry for each task object.
-        Simply grab 1st free Task in database and run crawler for its owner.
-        """
-        task = self.task_adapter.get_active_task()
-        print(task)
-        if task:
-            task = self.task_adapter.mark_task_taken(task)
-            crawler = self.crawler(initial_url=task.owner.url, initial_domain=task.owner.value)
-            try:
-                print(crawler)
-                asyncio.run(crawler.start_crawling())
-                task = self.task_adapter.mark_task_finished(task)
-            except Exception as e:
-                print(f'EXCEPTION: {e}')
 
 
 class Command(BaseCommand):
     """Base command for restarting Celery workers."""
 
     def handle(self, *args, **kwargs):
-        CrawlerLauncher().start()
+        CrawlerLauncher().launch()

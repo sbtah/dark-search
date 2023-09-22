fed91ab8c5bd275f76215cd48bd607653c5784f4
Sbtah <shisastore@protonmail.com>
2023-09-22 20:39:26 +0200

    Add prepare management command to help with initial setup.

---
+++ scout/core/management/commands/prepare.py
@@ -0,0 +1,62 @@
+from django.core.management.base import BaseCommand
+from client.api import TorScoutApiClient
+from logic.adapters.domain import DomainAdapter
+from logic.adapters.task import TaskAdapter
+from tasks.models import Task
+import time
+
+
+DOMAINS = [
+    'tor66sewebgixwhcqfnp5inzp5x5uohhdy3kvtnyfxc2e5mxiuh34iid.onion',
+    'hiddenmxictmv5wa4ljofvqiuskfjkxah2gvdpxxwbdmenufchlmooad.onion',
+]
+
+
+def prepare_launch():
+    """"""
+    for domain in DOMAINS:
+        domain_object = DomainAdapter().sget_or_create_domain(domain=domain)
+        task_object = TaskAdapter().sget_or_create_task(domain=domain_object)
+
+
+class Command(BaseCommand):
+    """"""
+
+    def handle(self, *args, **kwargs):
+        """"""
+        
+        self.stdout.write(self.style.SUCCESS(
+            """
+            Waiting for API to accept connections...
+            """
+        ))
+        api = False
+
+        while api is False:
+            try:
+                response = TorScoutApiClient().ping_home()
+                if response:
+                    if response.status_code == 200:
+                        api = True
+                else:
+                    time.sleep(3)
+            except Exception:
+                self.stderr.write(self.style.ERROR(
+                    """
+                    API not Available, waiting...
+                    """
+                ))
+                time.sleep(3)
+        else:
+            self.stderr.write(self.style.SUCCESS(
+                """
+                API is Available!
+                """
+            ))
+
+        prepare_launch()
+        self.stderr.write(self.style.SUCCESS(
+                f"""
+                Current number of Tasks in database: {Task.objects.all().count()}
+                """
+            ))

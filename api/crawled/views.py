2864f480f19ccb9c0d2d3d9f10181e4a72829d7f
Sbtah <shisastore@protonmail.com>
2023-09-22 20:30:05 +0200

    Refactor process_response view. Add process_summary view.

--- api/crawled/views.py
+++ api/crawled/views.py
@@ -10,28 +10,29 @@ from rest_framework.decorators import (
 )
 
 
-@api_view(["GET"])
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
+    """"""
     message = {'message': 'Hi, This is Tor Search API.'}
     return JsonResponse(message)
 
 
-@api_view(["POST"])
+@api_view(['POST'])
 def process_response(request, *args, **kwargs):
+    """
+    Process incoming response data from crawlers.
+    """
 
     domain_adapter = DomainAdapter()
     webpage_adapter = WebpageAdapter()
 
     response_data = json.loads(request.data)
-
     # Extracting domain from requested url.
     requested_domain = webpage_adapter.get_domain(
         response_data['requested_url'],
     )
     domain = domain_adapter.update_or_create_domain(
-        domain=requested_domain,
-        title=response_data.get('meta_data').get('title') if response_data.get('meta_data') is not None else None,
-        description=response_data.get('meta_data').get('description') if response_data.get('meta_data') is not None else None,
+        value=requested_domain,
         server=response_data.get('server'),
     )
 
@@ -42,12 +43,13 @@ def process_response(request, *args, **kwargs):
             url_after_request=response_data['responded_url'],
             last_http_status=response_data['status'],
             last_elapsed=response_data['elapsed'],
-            title=response_data['meta_data']['title'],
+            raw_html=response_data['raw_html'],
+            page_title=response_data['page_title'],
+            meta_title=response_data['meta_data']['title'],
             meta_description=response_data['meta_data']['description'],
-            visited=response_data['visited'],
+            last_visit=response_data['visited'],
             on_page_raw_urls=response_data['raw_urls'],
             on_page_processed_urls=response_data['processed_urls']
-
         )
         return JsonResponse(
             {'status': f'Parsed response: {webpage.url}'}
@@ -60,3 +62,21 @@ def process_response(request, *args, **kwargs):
         return JsonResponse(
             {'status': f'Parsed empty response: {webpage.url}'}
         )
+
+@api_view(['POST'])
+def process_summary(request, *args, **kwargs):
+    """
+    Process summary data after finished crawling.
+    """
+
+    domain_adapter = DomainAdapter()
+    response_data = json.loads(request.data)
+
+    domain = domain_adapter.update_or_create_domain(
+        value=response_data['domain'],
+        last_crawl_date=response_data['date'],
+        last_crawl_time=response_data['time'],
+        number_of_pages_found=response_data['urls_crawled'],
+        number_of_crawls_finished=1
+    )
+    return JsonResponse({'status': f'Updated {domain}'})

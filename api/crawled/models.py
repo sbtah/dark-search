d68ee05b2f8dda4f6ea53438a589dc2f5c83963a
Sbtah <shisastore@protonmail.com>
2024-05-14 23:05:50 +0200

    Update crawled models.

--- api/crawled/models.py
+++ api/crawled/models.py
@@ -24,7 +24,7 @@ class Entity(models.Model):
     Object representing organization or owner of TOR operation.
     Entity can own many Domains.
     """
-    # Entity can own many domains, basically serving same site.
+    # Entity can own many domains, basically serving same or many different domains.
     name = models.CharField(max_length=255, unique=True, db_index=True)
     # Description for entity, can be set manually or scraped from any source.
     description = models.TextField(blank=True, null=True)
@@ -49,15 +49,16 @@ class Domain(models.Model):
         Entity, on_delete=models.SET_NULL, blank=True, null=True
     )
     value = models.CharField(max_length=2000, unique=True, db_index=True)
-    created = models.IntegerField(blank=True, null=True)
-    last_crawl_date = models.IntegerField(blank=True, null=True)
-    average_crawl_time = models.IntegerField(blank=True, null=True)
     server = models.CharField(max_length=100, blank=True, null=True)
+    created = models.IntegerField(blank=True, null=True)
+    last_crawl_date = models.IntegerField(default=0)
+    average_crawl_time = models.IntegerField(default=0)
     description = models.TextField(blank=True, null=True)
     additional_data = models.JSONField(blank=True, null=True)
-    # New:
-    number_of_crawls_finished = models.IntegerField(default=0)
-    number_of_webpages = models.IntegerField(default=0)
+    # Will increment on crawl start.
+    number_of_crawls = models.IntegerField(default=0)
+    # Will increment on crawl end.
+    number_of_successful_crawls = models.IntegerField(default=0)
     # What is this Domain about.
     description_tags = models.ManyToManyField(Tag)
     site_structure = models.JSONField(blank=True, null=True)
@@ -81,24 +82,32 @@ class Webpage(models.Model):
     """
     parent_domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
     url = models.URLField(max_length=2000, unique=True, db_index=True)
-    # Since we could get redirected.
+    # Since we could get redirected. This url does not have to be unique.
     url_after_request = models.URLField(max_length=2000)
+    last_request_date = models.IntegerField(default=0)
     last_http_status = models.CharField(max_length=3, blank=True, null=True)
-    average_response_time = models.FloatField(blank=True, null=True)
+    # Calculated for successful responses.
+    average_response_time = models.FloatField(default=0)
     html = models.TextField(blank=True, null=True)
+    # h1 tag...
     page_title = models.CharField(max_length=2000, blank=True, null=True, db_index=True)
     meta_title = models.CharField(max_length=2000, blank=True, null=True)
-    meta_description = models.TextField(blank=True, null=True)
+    meta_description = models.TextField(blank=True, null=True, db_index=True)
     additional_data = models.JSONField(blank=True, null=True)
     # What is this Webpage about.
     description_tags = models.ManyToManyField(Tag)
+    # Raw urls found.
     on_page_urls = ArrayField(models.URLField(max_length=2000), null=True, blank=True)
     on_page_processed_urls = ArrayField(models.URLField(max_length=2000), null=True, blank=True)
-    # How many times we successfully requested this url?
-    number_of_successful_requests = models.IntegerField(blank=True, null=True)
-    number_of_unsuccessful_requests = models.IntegerField(blank=True, null=True)
+    # How many times we successfully requested this url? status 200.
+    number_of_successful_requests = models.IntegerField(default=0)
+    number_of_unsuccessful_requests = models.IntegerField(default=0)
     is_active = models.BooleanField(default=False)
     created = models.IntegerField(blank=True, null=True)
+
+    # Ideas for identifiers:
+    # - Head hash
+    # - base64 favicon - on domain ?
 
     class Meta:
         db_table = "webpages"

bc3b9681db5b17c50efd242974dc9719654f0d2f
Sbtah <shisastore@protonmail.com>
2024-05-17 18:25:16 +0200

    Add tests for Domain.

--- api/tests/test_domain_model.py
+++ api/tests/test_domain_model.py
@@ -3,3 +3,36 @@ from crawled.models import Domain
 
 
 pytestmark = pytest.mark.django_db
+
+
+class TestDomainObject:
+    """Test cases for Domain model."""
+
+    def test_create_domain(self):
+        """Test creating Domain is successful."""
+        assert Domain.objects.count() == 0
+        domain = Domain.objects.create(
+            value='test.com',
+        )
+        assert Domain.objects.count() == 1
+
+    def test_domain_save_method(self):
+        """Test that Domain's save method is properly saving data only on creation"""
+        assert Domain.objects.count() == 0
+        domain = Domain.objects.create(
+            value='test.com',
+        )
+        current_created = domain.created
+        assert current_created is not None
+        domain.value = 'new-test.com'
+        domain.save()
+        domain.refresh_from_db()
+        assert current_created == domain.created
+        assert isinstance(current_created, int)
+
+    def test_domain_str_method(self):
+        """Test that Domain's str method is generating proper output."""
+        domain = Domain.objects.create(
+            value='test.com',
+        )
+        assert str(domain) == domain.value

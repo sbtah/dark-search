973ce578c1fde2f9e6d85d91766a4096a60e256c
Sbtah <shisastore@protonmail.com>
2024-05-17 22:33:48 +0200

    Add example_domain fixture. Change name for entity fixture to example_entity.

--- api/conftest.py
+++ api/conftest.py
@@ -2,9 +2,13 @@
 Pytest fixtures for api service.
 """
 import pytest
-from crawled.models import Entity
+from crawled.models import Entity, Domain
 
 
 @pytest.fixture
-def entity():
+def example_entity():
     return Entity.objects.create(name='test-entity')
+
+@pytest.fixture
+def example_domain():
+    return Domain.objects.create(value='test.com')

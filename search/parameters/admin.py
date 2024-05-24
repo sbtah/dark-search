362b6b1219b6eda1d632a1e0ea63a507d639594d
Sbtah <shisastore@protonmail.com>
2024-05-24 10:06:16 +0200

    Register Proxy model in admin.py.

--- search/parameters/admin.py
+++ search/parameters/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from parameters.models import UserAgent
+from parameters.models import UserAgent, Proxy
 
 
 admin.site.register(UserAgent)
+admin.site.register(Proxy)

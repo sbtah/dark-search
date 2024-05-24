bac1a677e429c8047377e919bd166cbe0ac7eaf6
Sbtah <shisastore@protonmail.com>
2024-05-24 12:44:49 +0200

    Remove debuging print statements. Fix doc strings. Imrove type returned by run_requests.

--- search/logic/spiders/asynchronous.py
+++ search/logic/spiders/asynchronous.py
@@ -1,6 +1,6 @@
 import asyncio
 from collections import deque
-from typing import Iterable
+from typing import Iterable, Any
 
 import httpx
 from httpx import Response
@@ -19,7 +19,8 @@ class AsyncSpider(BaseSpider):
 
     async def get(self, url: str) -> Response | None:
         """
-        Request specified URL. Return Response object on success.
+        Request specified URL.
+        Return the Response object on success.
         - :arg url: Requested URL.
         """
         headers = self.prepare_headers()
@@ -34,9 +35,9 @@ class AsyncSpider(BaseSpider):
             self.logger.error(f'(get) Some other exception: {e}')
             return None
 
-    async def run_requests(self, iterable_of_urls: Iterable) -> deque[dict]:
+    async def run_requests(self, iterable_of_urls: Iterable) -> tuple[BaseException | Any] | None:
         """
-        Send requests to collection of urls.
+        Send requests to the collection of urls.
         - :arg iterable_of_urls: Iterable of Urls that we can loop over.
         """
         # o(1) complexity
@@ -59,7 +60,7 @@ class AsyncSpider(BaseSpider):
     async def request(self, url: str) -> dict:
         """
         Request specified url asynchronously.
-        Returns dictionary with needed data.
+        Return dictionary with needed data.
         """
         # Response from requesting a webpage.
         response = await self.get(url=url)
@@ -88,15 +89,10 @@ class AsyncSpider(BaseSpider):
 
         # Extract data from requested page.
         page_title: str | None = self.extract_page_title(html_element=element)
-        # print(f'DEBUG TITLE : {page_title}')
         meta_data: dict | None = self.extract_meta_data(html_element=element)
-        # print(f'DEBUG META : {meta_data}')
         raw_urls: list = self.extract_urls(html_element=element)
-        # print(f'DEBUG RAW URLS : {raw_urls}')
         cleaned_html_body: str = self.sanitize_html_body(html_element=element)
-        # print(f'DEBUG HTML: {cleaned_html_body}')
         processed_urls: dict = self.url_extractor.parse(urls_collection=raw_urls)
-        # print(f'DEBUG processed URLS: {processed_urls}')
         html_parsed_response = {
             'html': cleaned_html_body,
             'page_title': page_title,
@@ -104,5 +100,4 @@ class AsyncSpider(BaseSpider):
             'raw_urls': raw_urls,
             'processed_urls': processed_urls,
         }
-
         return {**parsed_response, **extra_parsed_response, **html_parsed_response}

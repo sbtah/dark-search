7ac52a575d46b8375630cb1dcf41cf7de46f541e
Sbtah <shisastore@protonmail.com>
2024-05-27 19:28:24 +0200

    Modify BaseSpider. Remove html parsing logic. Add HtmlExtractor class as a composite.

--- search/logic/spiders/base.py
+++ search/logic/spiders/base.py
@@ -1,13 +1,11 @@
 import time
 from logging import Logger
 from urllib.parse import urlsplit
-
-from httpx import Response
 from logic.adapters.task import CrawlTaskAdapter
 from logic.parsers.url import UrlExtractor
-from lxml.html import HtmlElement, HTMLParser, fromstring, tostring
-from lxml.html.clean import Cleaner
+from logic.parsers.html import HtmlExtractor
 from utilities.logging import logger
+
 
 
 class BaseSpider:
@@ -22,6 +20,7 @@ class BaseSpider:
         self.user_agent: str = user_agent
         self.task_adapter: CrawlTaskAdapter = CrawlTaskAdapter()
         self.url_extractor: UrlExtractor = UrlExtractor(starting_url=initial_url)
+        self.html_extractor: HtmlExtractor = HtmlExtractor()
         self.logger: Logger = logger
 
     @staticmethod
@@ -51,92 +50,3 @@ class BaseSpider:
             'Connection': 'close',
             'User-Agent': self.user_agent,
         }
-
-    def page(self, response: Response) -> HtmlElement | None:
-        """
-        Parse response object and return HtmlElement on success.
-        - :arg response: httpx Response object.
-        """
-        try:
-            hp = HTMLParser(encoding='utf-8')
-            element = fromstring(
-                response.text,
-                parser=hp,
-            )
-            return element
-        except Exception as e:
-            self.logger.exception(f'Exception while generating HtmlElement: {e}')
-            return None
-
-    def extract_urls(self, html_element: HtmlElement) -> list[str] | None:
-        """
-        Search for urls in body of provided HtmlElement.
-        - :arg html_element: Lxml HtmlElement.
-        """
-        urls = html_element.xpath('/html/body//a[@href and not(@href="")]/@href')
-        if urls:
-            return [url.strip() for url in urls]
-        return None
-
-    def extract_favicon_url(self, html_element: HtmlElement) -> str | None:
-        """
-        Search for possible favicon in head of requested page.
-        - :arg html_element: Lxml HtmlElement.
-        """
-        # Sometimes this may be a list of urls with different icon for different resolutions.
-        favicon_urls = html_element.xpath('/head/link[contains(@href, "favicon")]/@href')
-        if favicon_urls:
-            return favicon_urls[0].strip()
-        return None
-
-    def extract_meta_data(self, html_element: HtmlElement) -> dict:
-        """
-        Take HtmlElement as an input,
-            return title and meta description from requested Webpage.
-        - :arg html_element: Lxml HtmlElement.
-        """
-        title_element = html_element.xpath('/html/head/title/text()')
-        description_element = html_element.xpath('/html/head/meta[@name="description"]/@content')
-        meta_data = {
-            'title': title_element[0].strip() if title_element else '',
-            'description': description_element[0].strip() if description_element else ''
-        }
-        return meta_data
-
-    def extract_page_title(self, html_element: HtmlElement) -> str | None:
-        """
-        Extract h1 text content from requested webpage.
-        Sometimes h1 can contain other nested elements,
-            because of that we want to extract entire text contained within h1 tag.
-        Return parsed content.
-        - :arg html_element: Lxml HtmlElement.
-        """
-        h1 = html_element.xpath('.//h1')
-        if not h1:
-            return None
-        page_title = h1[0].text_content().strip()
-        return page_title
-
-    def sanitize_html_body(self, html_element: HtmlElement) -> str | None:
-        """
-        Extract body part from HtmlElement.
-        Clean html of dangerous elements and attributes.
-        """
-        body = html_element.xpath('/html/body')
-        cleaner = Cleaner(
-                style=True,
-                inline_style=True,
-                scripts=True,
-                javascript=True,
-                embedded=True,
-                frames=True,
-                meta=True,
-                annoying_tags=True,
-                kill_tags=['img']
-            )
-        try:
-            sanitized_content = tostring(cleaner.clean_html(body[0]))
-        except Exception as e:
-            self.logger.exception(f'Exception while cleaning HtmlElement: {e}')
-            sanitized_content = b''
-        return sanitized_content.decode('utf-8')

58aca063bf7b0651907432b1a13a19d004266e1e
Sbtah <shisastore@protonmail.com>
2024-06-09 09:46:25 +0200

    Add excpetion for Url creation validation.

--- search/logic/exceptions/adapters/url.py
+++ search/logic/exceptions/adapters/url.py
@@ -1,5 +1,11 @@
 class WrongTypeProvidedError(Exception):
     """Exception raised while validating types on creation of Url objects."""
-    def __init__(self, message="Wrong type given for value"):
+    def __init__(self, message='Wrong type provided for Url attribute.'):
+        self.message = message
+        super().__init__(self.message)
+
+class WrongValueProvidedError(Exception):
+    """Exception raised while validating values on creation of Url objects."""
+    def __init__(self, message='Wrong value provided for Url attribute.'):
         self.message = message
         super().__init__(self.message)

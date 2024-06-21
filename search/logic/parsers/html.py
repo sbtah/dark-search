e3ddb42c65ae0f4e94ef48a90b53183d935a3641
Sbtah <shisastore@protonmail.com>
2024-06-21 08:11:33 +0200

    Add cleaner to html parser.

--- search/logic/parsers/html.py
+++ search/logic/parsers/html.py
@@ -1,6 +1,7 @@
 from httpx import Response
 from logic.parsers.objects.url import Url
 from lxml.html import HtmlElement, HTMLParser, fromstring, tostring
+from lxml.html.clean import Cleaner
 
 
 class HtmlExtractor:
@@ -13,7 +14,7 @@ class HtmlExtractor:
     - On page urls with texts.
     """
 
-    def parse(self, html_element: HtmlElement, favicon: bool=True) -> dict:
+    def parse(self, html_element: HtmlElement, favicon: bool = True) -> dict:
         """
         Parse HtmlElement.
         Return dictionary with extracted data.
@@ -37,7 +38,8 @@ class HtmlExtractor:
             result['favicon_url'] = favicon_url
         return result
 
-    def page(self, response: Response) -> HtmlElement | None:
+    @staticmethod
+    def page(response: Response) -> HtmlElement | None:
         """
         Parse response object and return HtmlElement on success.
         - :arg response: httpx Response object.
@@ -52,7 +54,8 @@ class HtmlExtractor:
         except Exception:
             return None
 
-    def extract_urls(self, html_element: HtmlElement) -> list[str] | None:
+    @staticmethod
+    def extract_urls(html_element: HtmlElement) -> list[str] | None:
         """
         Search for urls in body of provided HtmlElement.
         - :arg html_element: Lxml HtmlElement.
@@ -60,17 +63,19 @@ class HtmlExtractor:
         urls: list[str | None] = html_element.xpath('/html/body//a[@href and not(@href="")]/@href')
         return [url.strip() for url in urls] if urls else None
 
-
-    def extract_urls_with_texts(self, html_element: HtmlElement) -> list[dict[str: str, str: str]] | None:
+    @staticmethod
+    def extract_urls_with_texts(html_element: HtmlElement) -> list[dict[str: str, str: str]] | None:
         """
         Search for urls in body of provided HtmlElement.
         - :arg html_element: Lxml HtmlElement.
         """
         urls: list[HtmlElement | None] = html_element.xpath('/html/body//a[@href and not(@href="") and not(@href=" ")]')
-        return [{'url': url.xpath('./@href')[0], 'anchor': url.text_content().strip()} for url in urls] if urls else None
+        return [
+            {'url': url.xpath('./@href')[0], 'anchor': url.text_content().strip()} for url in urls
+        ] if urls else None
 
-
-    def extract_favicon_url(self, html_element: HtmlElement) -> str | None:
+    @staticmethod
+    def extract_favicon_url(html_element: HtmlElement) -> str | None:
         """
         Search for possible favicon in head of requested page.
         - :arg html_element: Lxml HtmlElement.
@@ -80,8 +85,8 @@ class HtmlExtractor:
         # We want first url and usually 1st url if the smallest one in terms of resolution.
         return favicon_urls[0].strip() if favicon_urls else None
 
-
-    def extract_page_title(self, html_element: HtmlElement) -> str | None:
+    @staticmethod
+    def extract_page_title(html_element: HtmlElement) -> str | None:
         """
         Extract h1 text content from requested webpage.
         Sometimes h1 can contain other nested elements,
@@ -92,8 +97,8 @@ class HtmlExtractor:
         h1: list[HtmlElement | None] = html_element.xpath('.//h1')
         return h1[0].text_content().strip() if h1 else None
 
-
-    def extract_meta_title(self, html_element: HtmlElement) -> str | None:
+    @staticmethod
+    def extract_meta_title(html_element: HtmlElement) -> str | None:
         """
         Extract meta-title content from the requested webpage.
         Return parsed content.
@@ -102,7 +107,8 @@ class HtmlExtractor:
         title_element: list[HtmlElement | None] = html_element.xpath('/html/head/title/text()')
         return title_element[0].strip() if title_element else None
 
-    def extract_meta_description(self, html_element: HtmlElement) -> str | None:
+    @staticmethod
+    def extract_meta_description(html_element: HtmlElement) -> str | None:
         """
         Extract description content from the requested webpage.
         Return parsed content.
@@ -111,13 +117,27 @@ class HtmlExtractor:
         description_element: list[str | None] = html_element.xpath('/html/head/meta[@name="description"]/@content')
         return description_element[0].strip() if description_element else None
 
-    def extract_entire_text(self, html_element: HtmlElement) -> str | None:
+    @staticmethod
+    def extract_entire_text(html_element: HtmlElement) -> str | None:
         """
         Extract body from HtmlElement.
-        Return entire text from all nodes on success. 
+        Clean html of dangerous elements and attributes
+        Return entire text from all nodes on success.
         - :arg html_element: Lxml HtmlElement.
         """
         body: list[HtmlElement | None] = html_element.xpath('/html/body')
-        content: str | None = body[0].text_content() if body else None
+        cleaner = Cleaner(
+            style=True,
+            inline_style=True,
+            scripts=True,
+            javascript=True,
+            embedded=True,
+            frames=True,
+            meta=True,
+            annoying_tags=True,
+        )
+        try:
+            content: str | None = cleaner.clean_html(body[0]).text_content() if body else None
+        except Exception:
+            content: None = None
         return content
-

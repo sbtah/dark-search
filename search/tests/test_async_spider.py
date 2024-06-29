4368fb0635e7d31dea9490ab3e385f294cddd2b5
Sbtah <shisastore@protonmail.com>
2024-06-29 20:56:34 +0200

    Add more tests for AsyncSpider.

--- search/tests/test_async_spider.py
+++ search/tests/test_async_spider.py
@@ -25,8 +25,8 @@ def spider(example_url_object):
 class TestAsyncSpider:
     """Test cases for AsyncSpider class."""
 
-    @patch('logic.spiders.asynchronous.httpx.AsyncClient.get')
     @pytest.mark.asyncio
+    @patch('logic.spiders.asynchronous.httpx.AsyncClient.get')
     async def test_async_spider_get_method_returns_response_and_url_object(
         self,
         mock_get,
@@ -35,7 +35,7 @@ class TestAsyncSpider:
         example_url_object,
     ):
         """
-        Test that AsyncSpider get method is returning tuple with Response and Url objects.
+        Test that AsyncSpider get method is returning tuple with Response and Url object.
         """
         mock_respone = MagicMock(spec=Response, status_code=200)
         mock_get.return_value = mock_respone, example_url_object
@@ -46,3 +46,98 @@ class TestAsyncSpider:
         assert len(response) == 2
         assert response[1].number_of_requests == 1
 
+
+    @pytest.mark.asyncio
+    @patch('logic.spiders.asynchronous.httpx.AsyncClient.get')
+    async def test_async_spider_get_method_returns_none_on_exception(
+        self, mock_get, spider, example_url_object,
+    ):
+        """
+        Test that AsyncSpider get method is returning tuple with None and Url object on exception.
+        """
+        mock_get.side_effect = HTTPError('Error!')
+
+        response = await spider.get(url=spider.initial_url)
+        mock_get.assert_called_once()
+        assert isinstance(response, tuple)
+        assert len(response) == 2
+        assert response[1].number_of_requests == 1
+        assert response[0] is None
+
+    @pytest.mark.asyncio
+    @patch('logic.spiders.asynchronous.AsyncSpider.get')
+    async def test_async_spider_request_method_returns_proper_response(
+        self, mock_get, spider, example_text_response, example_url_object
+    ):
+        """
+        Test that AsyncSpider request method is returning desired response dictionary.
+        """
+        mock_response = MagicMock(spec=Response, status_code=200)
+        mock_response.text = example_text_response
+        mock_response.url = example_url_object.value
+        mock_response.headers = {'server': 'test_server'}
+        mock_response.elapsed = timedelta(seconds=10)
+        mock_get.return_value = mock_response, example_url_object
+
+        response = await spider.request(spider.initial_url)
+        mock_get.assert_called_once()
+
+        assert isinstance(response, dict)
+        assert {
+            'requested_url',
+            'responded_url',
+            'status',
+            'server',
+            'elapsed',
+            'visited',
+            'text',
+            'page_title',
+            'meta_title',
+            'meta_description',
+            'on_page_urls',
+            'processed_urls',
+        } == set(response.keys())
+
+    @pytest.mark.asyncio
+    @patch('logic.spiders.asynchronous.AsyncSpider.get')
+    async def test_async_spider_request_method_returns_desired_data_for_wrong_status_codes(
+        self, mock_get, spider, example_url_object,
+    ):
+        """
+        Test that AsyncSpider request method is returning desired response
+            when status codes are not in 200 or 300 range.
+        """
+        mock_response = MagicMock(spec=Response, status_code=404)
+        mock_response.text = ''
+        mock_response.url = example_url_object.value
+        mock_response.elapsed = timedelta(10)
+        mock_response.headers = {'server': 'test_server'}
+        mock_get.return_value = mock_response, example_url_object
+        response = await spider.request(spider.initial_url)
+        mock_get.assert_called_once()
+
+        assert isinstance(response, dict)
+        assert {
+            'requested_url',
+            'responded_url',
+            'status',
+            'server',
+            'elapsed',
+            'visited',
+        } == set(response.keys())
+
+    @pytest.mark.asyncio
+    @patch('logic.spiders.asynchronous.AsyncSpider.get')
+    async def test_async_spider_request_method_returns_desired_data_for_none_response(
+        self, mock_get, spider, example_url_object,
+    ):
+        """
+        Test that AsyncSpider request method is returning desired response when response is None.
+        """
+        mock_get.return_value = None, example_url_object
+        response = await spider.request(spider.initial_url)
+        mock_get.assert_called_once()
+
+        assert isinstance(response, dict)
+        assert {'requested_url', 'status'} == set(response.keys())
+        assert response['status'] is None

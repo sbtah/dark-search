813c95f583ac1a7edae536ddf1e9e356f3d63c6f
Sbtah <shisastore@protonmail.com>
2024-07-10 20:23:11 +0200

    Add sync_get_or_create_user_agent method on adapter.

--- search/logic/adapters/agents.py
+++ search/logic/adapters/agents.py
@@ -12,6 +12,26 @@ class UserAgentAdapter(BaseAdapter):
         self.agent = UserAgent
         super().__init__(*args, **kwargs)
 
+    def sync_get_or_create_user_agent(self, user_agent: str) -> UserAgent:
+        """
+        Create new UserAgent object or return existing one.
+        - :arg user_agent: String representing browser User Agent.
+        """
+        try:
+            existing_agent: UserAgent = self.agent.objects.get(value=user_agent)
+            self.logger.debug(
+                f'UserAgent Adapter, found existing User Agent: user_agent_id="{existing_agent.id}", '
+                f'value="{user_agent}"'
+            )
+            return existing_agent
+        except UserAgent.DoesNotExist:
+            new_agent: UserAgent = self.agent.objects.create(value=user_agent)
+            self.logger.debug(
+                f'UserAgent Adapter, created new User Agent: user_agent_id="{new_agent.id}", '
+                f'value="{user_agent}"'
+            )
+            return new_agent
+
     def get_random_user_agent(self) -> UserAgent:
         """
         Retrieve random UserAgent object from database.

642afa4fdaad23c0e4bcc58b535c89d49f891df6
Sbtah <shisastore@protonmail.com>
2024-07-10 20:37:06 +0200

    Add sync_get_or_create_proxy to adapter.

--- search/logic/adapters/proxy.py
+++ search/logic/adapters/proxy.py
@@ -10,6 +10,20 @@ class ProxyAdapter(BaseAdapter):
         self.proxy = Proxy
         super().__init__(*args, **kwargs)
 
+    def sync_get_or_create_proxy(self, proxy: str):
+        """
+        Create new Proxy object or return existing one.
+        - :arg proxy: String representing Proxy value.
+        """
+        try:
+            existing_proxy = self.proxy.objects.get(value=proxy)
+            self.logger.debug(f'Proxy Adapter, found existing Proxy: proxy_id="{existing_proxy.id}", value="{proxy}"')
+            return existing_proxy
+        except Proxy.DoesNotExist:
+            new_proxy = self.proxy.objects.create(value=proxy)
+            self.logger.debug(f'Proxy Adapter, created new Proxy: proxy_id="{new_proxy.id}", value="{proxy}"')
+            return new_proxy
+
     def get_proxy(self) -> Proxy:
         """Retrieve the least used Proxy from the database."""
         proxies = self.proxy.objects.all().order_by('current_spiders')

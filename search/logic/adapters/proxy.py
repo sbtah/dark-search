9105c37a9055e9e7197c903039d756a6572e649e
Sbtah <shisastore@protonmail.com>
2024-07-12 14:02:14 +0200

    Fix imports and type hints in ProxyAdapter.

--- search/logic/adapters/proxy.py
+++ search/logic/adapters/proxy.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+from django.db.models import QuerySet
 from logic.adapters.base import BaseAdapter
 from logic.exceptions.adapters.proxy import NoProxiesError
 from parameters.models import Proxy
@@ -6,27 +9,27 @@ from parameters.models import Proxy
 class ProxyAdapter(BaseAdapter):
     """Adapter for managing Proxy objects."""
 
-    def __init__(self, *args, **kwargs):
-        self.proxy = Proxy
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.proxy: Proxy = Proxy
         super().__init__(*args, **kwargs)
 
-    def sync_get_or_create_proxy(self, proxy: str):
+    def sync_get_or_create_proxy(self, proxy: str) -> Proxy:
         """
         Create new Proxy object or return existing one.
         - :arg proxy: String representing Proxy value.
         """
         try:
-            existing_proxy = self.proxy.objects.get(value=proxy)
+            existing_proxy: Proxy = self.proxy.objects.get(value=proxy)
             self.logger.debug(f'Proxy Adapter, found existing Proxy: proxy_id="{existing_proxy.id}", value="{proxy}"')
             return existing_proxy
         except Proxy.DoesNotExist:
-            new_proxy = self.proxy.objects.create(value=proxy)
+            new_proxy: Proxy = self.proxy.objects.create(value=proxy)
             self.logger.debug(f'Proxy Adapter, created new Proxy: proxy_id="{new_proxy.id}", value="{proxy}"')
             return new_proxy
 
     def get_proxy(self) -> Proxy:
         """Retrieve the least used Proxy from the database."""
-        proxies = self.proxy.objects.all().order_by('current_spiders')
+        proxies: QuerySet[Proxy | None] = self.proxy.objects.all().order_by('current_spiders')
         if not proxies:
             raise NoProxiesError()
         return proxies.first()

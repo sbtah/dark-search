f1591ae7f181b175dc70549ec40e6a0df6d24608
Sbtah <shisastore@protonmail.com>
2024-07-12 13:51:29 +0200

    Add types annotations on fields. Add index.

--- search/tasks/models.py
+++ search/tasks/models.py
@@ -4,14 +4,16 @@ from django.db import models
 class CrawlTask(models.Model):
     """Base object for a crawl task."""
 
-    domain = models.CharField(max_length=255, unique=True, blank=False, default=None)
-    current_celery_id = models.CharField(max_length=100, blank=True, null=True)
-    last_launch_date = models.IntegerField(default=0)
-    last_finished_date = models.IntegerField(default=0)
-    number_of_launches = models.IntegerField(default=0)
-    number_of_finished_launches = models.IntegerField(default=0)
-    average_time_to_finish = models.IntegerField(default=0)
-    importance = models.IntegerField(default=0)
+    domain: models.CharField = models.CharField(
+        max_length=255, unique=True, blank=False, default=None, db_index=True
+    )
+    current_celery_id: models.CharField = models.CharField(max_length=100, blank=True, null=True)
+    last_launch_date: models.IntegerField = models.IntegerField(default=0, db_index=True)
+    last_finished_date: models.IntegerField = models.IntegerField(default=0, db_index=True)
+    number_of_launches: models.IntegerField = models.IntegerField(default=0)
+    number_of_finished_launches: models.IntegerField = models.IntegerField(default=0)
+    average_time_to_finish: models.IntegerField = models.IntegerField(default=0)
+    importance: models.IntegerField = models.IntegerField(default=0)
 
     class Status(models.TextChoices):
         active = 'ACTIVE'
@@ -28,13 +30,13 @@ class CrawlTask(models.Model):
         six = 6
         seven = 7
 
-    status = models.CharField(max_length=10, choices=Status.choices, default=Status.active)
-    frequency = models.IntegerField(choices=Frequency.choices, default=Frequency.one)
+    status: models.CharField = models.CharField(max_length=10, choices=Status.choices, default=Status.active)
+    frequency: models.IntegerField = models.IntegerField(choices=Frequency.choices, default=Frequency.one)
 
     class Meta:
         db_table = 'crawl_tasks'
         db_table_comment = 'Task data and statistics.'
         verbose_name_plural = 'CrawlTasks'
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.__class__.__name__}:{str(self.domain)}'

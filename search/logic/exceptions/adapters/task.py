e7c8cc8fe0b2a9411fa8755c68149f741afdc405
Sbtah <shisastore@protonmail.com>
2024-07-12 14:07:42 +0200

    Add type hints for Task Exceptions.

--- search/logic/exceptions/adapters/task.py
+++ search/logic/exceptions/adapters/task.py
@@ -1,12 +1,12 @@
 class NoActiveTasksError(Exception):
     """Exception raised when queryset for tasks is empty."""
-    def __init__(self, message='No tasks found in database.'):
+    def __init__(self, message: str = 'No tasks found in database.') -> None:
         self.message = message
         super().__init__(self.message)
 
 
 class NoTaskValueProvidedError(Exception):
     """Exception raised when one of values needed for saving a Task is missing."""
-    def __init__(self, message='No required value provided.'):
+    def __init__(self, message: str = 'No required value provided.') -> None:
         self.message = message
         super().__init__(self.message)
--- search/logic/exceptions/adapters/url.py
+++ search/logic/exceptions/adapters/url.py
@@ -1,11 +1,12 @@
 class WrongTypeProvidedError(Exception):
     """Exception raised while validating types on creation of Url objects."""
-    def __init__(self, message='Wrong type provided for Url attribute.'):
+    def __init__(self, message: str = 'Wrong type provided for Url attribute.') -> None:
         self.message = message
         super().__init__(self.message)
 
+
 class WrongValueProvidedError(Exception):
     """Exception raised while validating values on creation of Url objects."""
-    def __init__(self, message='Wrong value provided for Url attribute.'):
+    def __init__(self, message: str = 'Wrong value provided for Url attribute.') -> None:
         self.message = message
         super().__init__(self.message)

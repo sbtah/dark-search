546cfc2956033ba46e6b661f3292a89b11f62dc6
Sbtah <shisastore@protonmail.com>
2024-07-12 14:13:53 +0200

    Add type hints in Url class.

--- search/logic/parsers/objects/url.py
+++ search/logic/parsers/objects/url.py
@@ -1,21 +1,23 @@
 class Url:
     """Hashable object representing url."""
 
-    def __init__(self, value: str, anchor: str = '', number_of_requests: int = 0):
+    def __init__(self, value: str, anchor: str = '', number_of_requests: int = 0) -> None:
         self.value: str = value
         self.anchor: str = anchor
         self.number_of_requests: int = number_of_requests
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.value
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.value
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(str(self.value))
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Url):
+            return NotImplemented
         return self.value == other.value
 
     def serialize(self) -> dict:

900957b8318e635c3b1be917140d8c4b16d2358b
Sbtah <shisastore@protonmail.com>
2024-07-12 14:04:10 +0200

    Add types for Proxy Exceptions.

--- search/logic/exceptions/adapters/proxy.py
+++ search/logic/exceptions/adapters/proxy.py
@@ -1,5 +1,5 @@
 class NoProxiesError(Exception):
     """Exception raised when queryset for proxies is empty."""
-    def __init__(self, message="No proxies found in database."):
+    def __init__(self, message: str = "No proxies found in database.") -> None:
         self.message = message
         super().__init__(self.message)

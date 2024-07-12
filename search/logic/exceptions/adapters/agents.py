a11f51b355e3e5a553a1a15592c500c5bbd806ac
Sbtah <shisastore@protonmail.com>
2024-07-12 13:54:31 +0200

    Add types in UseAgents Exceptions.

--- search/logic/exceptions/adapters/agents.py
+++ search/logic/exceptions/adapters/agents.py
@@ -1,5 +1,5 @@
 class NoUserAgentsError(Exception):
     """Exception raised when queryset of agents is empty."""
-    def __init__(self, message="No user agents found in database."):
+    def __init__(self, message: str = "No user agents found in database.") -> None:
         self.message = message
         super().__init__(self.message)

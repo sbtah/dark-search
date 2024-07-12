939859b2343ae5ab41a13572ffa8b6f4da3e8fcc
Sbtah <shisastore@protonmail.com>
2024-07-12 13:59:39 +0200

    Fix imports. Add return types for fixtures.

--- search/conftest.py
+++ search/conftest.py
@@ -3,20 +3,20 @@ Pytest fixtures
 """
 import pytest
 from logic.adapters.url import UrlAdapter
-from logic.parsers.url import UrlExtractor
-from lxml.html import fromstring
-from parameters.models import Proxy, UserAgent
 from logic.parsers.objects.url import Url
+from logic.parsers.url import UrlExtractor
+from lxml.html import HtmlElement, fromstring
+from parameters.models import Proxy, UserAgent
 
 
 @pytest.fixture
-def url_extractor():
+def url_extractor() -> UrlExtractor:
     starting_url = UrlAdapter.create_url_object(value='http://example.onion')
     return UrlExtractor(starting_url)
 
 
 @pytest.fixture
-def urls_collection():
+def urls_collection() -> list[dict[str, str]]:
     urls_collection = [
         {'url': 'http://example.onion/page', 'anchor': 'Url 1'},
         {'url': 'http://example.onion/path?page=1', 'anchor': 'Url 2'},
@@ -39,24 +39,24 @@ def urls_collection():
 
 
 @pytest.fixture
-def user_agent():
+def user_agent() -> UserAgent:
     return UserAgent.objects.create(value='Mozilla/5.0 Test Agent')
 
 
 @pytest.fixture
-def many_agents():
+def many_agents() -> None:
     for _ in range (1, 11):
         UserAgent.objects.create(value=f'Mozilla/5.0 Test Agent {_}')
 
 
 @pytest.fixture
-def many_proxies():
+def many_proxies() -> None:
     for _ in range(1, 11):
         Proxy.objects.create(value=f'127.0.0.{_}', current_spiders=_)
 
 
 @pytest.fixture
-def many_urls_element():
+def many_urls_element() -> HtmlElement:
     return fromstring(
         '<html>'
             '<head></head>'
@@ -68,7 +68,7 @@ def many_urls_element():
 
 
 @pytest.fixture
-def empty_urls_element():
+def empty_urls_element() -> HtmlElement:
     return fromstring(
         '<html>'
             '<head></head>'
@@ -80,7 +80,7 @@ def empty_urls_element():
 
 
 @pytest.fixture
-def empty_texts_urls_element():
+def empty_texts_urls_element() -> HtmlElement:
     return fromstring(
         '<html>'
             '<head></head>'
@@ -92,40 +92,40 @@ def empty_texts_urls_element():
 
 
 @pytest.fixture
-def no_urls_element():
+def no_urls_element() -> HtmlElement:
     return fromstring(
         '<html><head></head><body><p>Test</p></body></html>'
     )
 
 
 @pytest.fixture
-def favicon_url_element():
+def favicon_url_element() -> HtmlElement:
     return fromstring(
         '<html><head><link href="/favicon.ico"></head><body><p>Test</p></body></html>'
     )
 
 
 @pytest.fixture
-def nested_h1_element():
+def nested_h1_element() -> HtmlElement:
     return fromstring(
         '<html><head></head><body><h1><div><p>This is a title</p></div></h1></body></html>'
     )
 
 @pytest.fixture
-def meta_title_element():
+def meta_title_element() -> HtmlElement:
     return fromstring(
         '<html><head><title>Test Page</title></head><body><p>Test</p></body></html>'
     )
 
 @pytest.fixture
-def meta_description_element():
+def meta_description_element() -> HtmlElement:
     return fromstring(
         '<html><head><meta name="description" content="Description!"></head><body><p>Test</p></body></html>'
     )
 
 
 @pytest.fixture
-def example_webpage_element():
+def example_webpage_element() -> HtmlElement:
     return fromstring(
         """
         <html>
@@ -142,7 +142,7 @@ def example_webpage_element():
 
 
 @pytest.fixture
-def example_text_response():
+def example_text_response() -> str:
     text = """
         <html>
             <head>
@@ -158,14 +158,13 @@ def example_text_response():
 
 
 @pytest.fixture
-def example_url_object():
+def example_url_object() -> Url:
     url = Url(value='http://found.onion/')
     return url
 
 
 @pytest.fixture
-def example_url_objects():
+def example_url_objects() -> list[Url]:
     """Collection of Url objects."""
     urls = [Url(value=f'http://found.onion/page{_}') for _ in range(5)]
     return urls
-

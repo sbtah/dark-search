0b0ef6a97413a89c5734a55a230f54f1b5a8eb6d
Sbtah <shisastore@protonmail.com>
2024-07-14 10:01:13 +0200

    Add collection_of_domains and example_linked_domain fixtures.

--- api/conftest.py
+++ api/conftest.py
@@ -9,9 +9,25 @@ from crawled.models import Entity, Domain, Webpage
 def example_entity():
     return Entity.objects.create(name='test-entity')
 
+
 @pytest.fixture
 def example_domain():
     return Domain.objects.create(value='test.com')
+
+
+@pytest.fixture
+def example_linked_domain(example_domain):
+    list_of_domains = [Domain.objects.create(value=f'test-{_}.onion') for _ in range(1, 11)]
+    for idx, domain in enumerate(list_of_domains):
+        if idx % 2 == 0:
+            domain.linking_to.add(example_domain)
+    return example_domain
+
+
+@pytest.fixture
+def collection_of_domains():
+    return ['domain1.onion', 'domain2.onion', 'domain3.onion', 'domain3.onion', 'domain4.onion']
+
 
 @pytest.fixture
 def example_webpage(example_domain):

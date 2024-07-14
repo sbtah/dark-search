b8d931df691acdc6ec11791b3a4d7d3f1021e29c
Sbtah <shisastore@protonmail.com>
2024-07-14 10:05:32 +0200

    Update tests for EntityAdapter.

--- api/tests/test_entity_adapter.py
+++ api/tests/test_entity_adapter.py
@@ -13,43 +13,47 @@ pytestmark = pytest.mark.django_db
 
 @pytest.fixture
 def adapter() -> EntityAdapter:
+    """
+    Fixture returning instance of EntityAdapter.
+    Logging is disabled in testing.
+    """
     adapter: EntityAdapter = EntityAdapter()
     adapter.logger = MagicMock()
     return adapter
 
 
 class TestEntityAdapter:
-    """Test cases for EntityAdapter functionality."""
+    """Test cases for EntityAdapter feature."""
 
-    def test_entity_adapter_sync_get_or_create_entity_getting_successful(
+    def test_entity_adapter_get_or_create_entity_is_return_existing_object(
         self,
         adapter,
         example_entity,
     ) -> None:
         """
-        Test that sync_get_or_create_entity is successfully fetching Entity objects from db.
+        Test that get_or_create_entity is successfully fetching an Entity object from db.
         """
         return_value = adapter.get_or_create_entity(name='test-entity')
         assert isinstance(return_value, Entity)
 
-    def test_entity_adapter_sync_get_or_create_entity_create_entity_with_name_only(
+    def test_entity_adapter_get_or_create_entity_create_entity_with_name_only(
         self,
         adapter,
     ) -> None:
         """
-        Test that sync_get_or_create_entity successfully creates a new Entity object.
+        Test that get_or_create_entity successfully creates a new Entity object.
         """
         assert Entity.objects.count() == 0
         return_value = adapter.get_or_create_entity(name='test-entity')
         assert Entity.objects.count() == 1
         assert return_value.name == 'test-entity'
 
-    def test_entity_adapter_sync_get_or_create_entity_create_entity_with_extra_data(
+    def test_entity_adapter_get_or_create_entity_create_entity_with_extra_data(
         self,
         adapter,
     ) -> None:
         """
-        Test that sync_get_or_create_entity is creating new Entity with all additional data.
+        Test that get_or_create_entity is creating new Entity with all additional data.
         """
         assert Entity.objects.count() == 0
         name: str = 'test-entity'

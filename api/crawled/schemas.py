cff153fccabeb84232482f8d7eadc674e42cf351
Sbtah <shisastore@protonmail.com>
2024-07-20 22:53:20 +0200

    Add schemas for Webpage objects JsonFields.

--- api/crawled/schemas.py
+++ api/crawled/schemas.py
@@ -1,7 +1,7 @@
-from ninja import Schema
+from pydantic import BaseModel, Field
 
 
-class ResponseSchema(Schema):
+class ResponseSchema(BaseModel):
     """
     Class representing a schema for Response received from crawler.
     """
@@ -19,7 +19,7 @@ class ResponseSchema(Schema):
     processed_urls: dict | None = None
 
 
-class SummarySchema(Schema):
+class SummarySchema(BaseModel):
     """
     Class representing a schema for payload received after finished crawl.
     """
@@ -28,3 +28,47 @@ class SummarySchema(Schema):
     # num_external_domains_found: int
     time: int
     date: int
+
+
+class SingleHttpStatusLogSchema(BaseModel):
+    """
+    Class representing a schema for single log in 'last_http_status_logs'.
+    """
+    date: str = Field(pattern=r'\d{2}-\d{2}-\d{4} \d{2}:\d{2}')
+    status: str = Field(pattern=r'\d{3}')
+
+
+class LastHttpStatusLogsSchema(BaseModel):
+    """
+    Class representing a schema for a Webpage JsonField 'last_http_status_logs'.
+    Example:
+    {
+        'status_logs': [
+            {'date': '22-07-2024 03:12', 'status': '404'},
+            {'date': '20-07-2024 13:48', 'status': '200'},
+        ]
+    }
+    """
+    status_logs: list[SingleHttpStatusLogSchema]
+
+
+class SingleLinkingToWebpagesLogSchema(BaseModel):
+    """
+    Class representing a schema for single log in 'lining_to_webpages_logs'.
+    """
+    date: str = Field(pattern=r'\d{2}-\d{2}-\d{4} \d{2}:\d{2}')
+    urls: list[str]
+
+
+class LinkingToWebpagesLogsSchema(BaseModel):
+    """
+    Class representing a schema for Webpage JsonField 'linking_to_webpages_logs'.
+    Example:
+    {
+        'webpages_logs':[
+            {'date': '20-11-2021', 'urls': ['https://test.onion.page1', 'http://test.onion/page-2']},
+            {'date': '20-11-2021', 'urls': ['https://test.onion.page1',]}
+        ]
+    }
+    """
+    webpages_logs: list[SingleLinkingToWebpagesLogSchema]

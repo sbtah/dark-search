bc38ea91b2f8304383eb49bfe4092ad1d206bf4d
marcinzygan <soul@o2.pl>
2024-08-07 20:07:28 +0200

    api setup

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -4,20 +4,22 @@ export default async function Home() {
   const API_URL = process.env.API_BASE_URL
   console.log(API_URL);
   
-
+let data
  try {
    const res = await fetch(`${API_URL}`)
-   const data = await res.json() 
+  data = await res.json() 
   console.log(data);
+  return data
  } catch (error) {
   console.log(error);
   
  }
 
- 
+ const {message} = data
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-Dark Search Main Page dups
+    Dark Search Main Page
+    API: {message}
     </main>
   );
 }

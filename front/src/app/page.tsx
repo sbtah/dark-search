78c2db1d987917ba91783f75e8ca81bcd903161c
marcinzygan <soul@o2.pl>
2024-08-07 20:22:26 +0200

    api setup

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,25 +1,22 @@
 
-
+type Data ={
+  message:string
+}
 export default async function Home() {
   const API_URL = process.env.API_BASE_URL
   console.log(API_URL);
   
-let data
- try {
-   const res = await fetch(`${API_URL}`)
-  data = await res.json() 
-  console.log(data);
-  return data
- } catch (error) {
-  console.log(error);
-  
- }
 
- const {message} = data
+ const devURL = "http://127.0.0.1:9003/api/"
+  const res = await fetch(`${API_URL}`)
+  const data = await res.json() 
+  console.log(data.message);
+ 
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Dark Search Main Page
-    API: {message}
+    Dark Search Main Page <br></br>
+    API Response: {data.message}
     </main>
   );
 }

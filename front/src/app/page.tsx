f4ad9f067e82782ab82e1cc8b2704a5f2ee1a7db
marcinzygan <soul@o2.pl>
2024-08-07 18:47:01 +0200

    project setup

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    MAIN PAGE DEV
+Dark Search Main Page
     </main>
   );
 }

da6848ca67db8a06ad15cbfff1db9d731b45513a
marcinzygan <soul@o2.pl>
2024-08-07 19:53:58 +0200

    api setup

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
 
-export default function Home() {
+
+export default async function Home() {
+  const API_URL = process.env.API_BASE_URL
+  console.log(API_URL);
+  
+
+ try {
+   const res = await fetch(`${API_URL}`)
+   const data = await res.json() 
+  console.log(data);
+ } catch (error) {
+  console.log(error);
+  
+ }
+
+ 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-Dark Search Main Page
+Dark Search Main Page d
     </main>
   );
 }

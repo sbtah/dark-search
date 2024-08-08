3f117ea3c786d49de2ee0b314e90b1b94545c240
marcinzygan <soul@o2.pl>
2024-08-08 19:23:48 +0200

    frontend setup

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,26 +1,22 @@
-
-type Data ={
-  current_num_of_domains: number
-  current_num_of_crawled_domains: number
-  current_num_of_webpages: number
-}
+type Data = {
+  current_num_of_domains: number;
+  current_num_of_crawled_domains: number;
+  current_num_of_webpages: number;
+};
 export default async function Home() {
-  const API_URL = process.env.API_BASE_URL
-  console.log(API_URL);
-  
+  // API URL ENV
+  const API_URL = process.env.API_BASE_URL;
 
-  const devURL = "http://127.0.0.1:9003/api/"
-  const res = await fetch(`${API_URL}`)
-  const data = await res.json()
+  const res = await fetch(`${API_URL}`);
+  const data: Data = await res.json();
   console.log(data);
- 
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Dark Search Main Page <br></br>
-    Domains known: {data.current_num_of_domains} <br></br>
-    Domains crawled: {data.current_num_of_crawled_domains} <br></br>
-    Webpages known: {data.current_num_of_domains}
+      Dark Search Main Page<br></br>
+      Domains known: {data.current_num_of_domains} <br></br>
+      Domains crawled: {data.current_num_of_crawled_domains} <br></br>
+      Webpages known: {data.current_num_of_domains}
     </main>
   );
 }
